5f5bd4ea671ac8ef7b8ed1430d1bd6afd02f5472
prathamesh <prathmesh6464@gmail.com>
2020-06-01 11:13:59 +0530

    REFACTORED : Changed start method added required import statements.

--- src/main/java/com/bridz/address_book/controller/AddressBookController.java
+++ src/main/java/com/bridz/address_book/controller/AddressBookController.java
@@ -1,12 +1,13 @@
 package com.bridz.address_book.controller;
 
 import com.bridz.address_book.service.AddressBookManager;
+import com.bridz.address_book.service.AddressBookManagerImplementation;
 
 public class AddressBookController {
 
 	public void start() {
-		AddressBookManager addressBookManager = new AddressBookManager();
-		addressBookManager.start();
+		AddressBookManager addressBookManager = new AddressBookManagerImplementation();
+		addressBookManager.displayAddressBookService();
 	}
 
 }
